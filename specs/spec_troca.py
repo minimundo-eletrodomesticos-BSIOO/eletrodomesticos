8fce85fd4b2d14fc684f449cf6ebc9509b19eedf
Alexandre Sampaio <allexonrails@gmail.com>
2012-10-29 19:17:39 -0200

    ajuste dramatico na garantia dos aparelhos

--- specs/spec_troca.py
+++ specs/spec_troca.py
@@ -5,6 +5,7 @@ sys.path.append('../app/')
 
 import unittest
 from should_dsl import should
+from datetime import *
 from produto import Produto
 from venda import Venda
 from cliente import Cliente
@@ -12,30 +13,26 @@ from troca import Troca
 
 class Test_Trocar_Spec(unittest.TestCase):
 	def setUp(self):
-		self.cliente = Cliente("Tu", "Tua Residencia")
+		self.cliente_1 = Cliente("Tu", "Tua Residencia")
+		self.cliente_2 = Cliente("Ela", "Residencia dela")
 		self.produto = Produto('Samsung Galaxy', 'SAMSUNG', 'I8150', 5)
 		self.produto.inserir_numeros_de_serie(["HWWx3", "HXD#F","LKJ#2","LKJ#3","L45#2"])
-		self.venda = Venda("Tu", "Tua Residencia",'Samsung Galaxy', 'SAMSUNG', 'I8150')
-		self.troca1 = Troca("26/11/2015", "Não liga","Tu", "Tua Residencia",'Samsung Galaxy', 'SAMSUNG', 'I8150','LKJ#2' )
-		self.troca2 = Troca("26/11/2012","Não liga","Tu", "Tua Residencia",'Samsung Galaxy', 'SAMSUNG', 'I8150','HXD#F' )
+		self.venda = Venda("Tu", "Tua Residencia",'Samsung Galaxy', 'SAMSUNG', 'I8150', "29/10/2012")
 		
-	
+		
 	def test_trocar_produto(self):
-		self.venda.fazer_venda(2)
-		self.venda.lista_vendas_2[0] |should| equal_to('HXD#F')
-		self.venda.lista_vendas_2[1] |should| equal_to('LKJ#2')
-		self.troca1.fazer_uma_troca()
-		
-		self.troca1.lista_troca[0].nome_cli |should| equal_to("Tu")
-		self.troca1.registro_de_trocados[0] |should| equal_to("L45#2")
-		
-	def test_trocar_nao_passou(self):
-		self.troca = Troca("26/11/2015", "Não liga","Tu", "Tua Residencia",'Samsung Galaxy', 'LG', 'I8150','LKJ#2' )
-		self.troca.fazer_uma_troca() |should| equal_to("Produto não encontrado")
-		
-		troca.registro_de_trocados[1] |should| equal_to('HWWx3')
+		self.venda.fazer_venda()
+		self.venda.fazer_venda()
+		troca = Troca("29/10/2012", "Ela","Não liga","Tu", "Tua Residencia",'Samsung Galaxy', 'SAMSUNG', 'I8150','LKJ#2' )
+		troca.fazer_uma_troca() |should| equal_to("Produto Trocado")
 	
+	def test_trocar_produto_nao_encontrado(self):
+		troca = Troca("29/10/2012", "Ela","Não liga","Tu", "Tua Residencia",'Samsung Galaxy', 'LG', 'I8150','LKJ#2' )
+		troca.fazer_uma_troca() |should| equal_to("Produto não encontrado")
 	
+	def test_trocar_produto_garantia_acabou(self):
+		troca = Troca("29/10/2015", "Ela","Não liga","Tu", "Tua Residencia",'Samsung Galaxy', 'SAMSUNG', 'I8150','LKJ#2' )
+		troca.fazer_uma_troca() |should| equal_to("Garantia acabou")
 
 if __name__ == "__main__":
     unittest.main()
--- specs/spec_venda.py
+++ specs/spec_venda.py
@@ -1,5 +1,6 @@
 #-*-coding:utf-8-*-
 import sys
+from datetime import *
 
 sys.path.append('../app/')
 
@@ -31,15 +32,16 @@ class Test_Venda_Spec(unittest.TestCase):
 		venda.fazer_venda() |should| equal_to("impossivel vender")
 	
 	def test_esta_na_garantia(self):
-		venda = Venda("Ela", "Casa dela","Samsung Galaxy", "SAMSUNG", "I8150", "26/10/2012")
-		venda.fazer_venda()
-		venda.lista_vendas_1[0].nome_cli |should| equal_to("Ela")
-		venda.lista_vendas_1[0].descricao_prod |should| equal_to('Samsung Galaxy')
-		venda.consultar_garantia("26/11/2012") |should| equal_to("Ainda está na garantia!")
+		self.venda = Venda("Ela", "Casa dela","Samsung Galaxy", "SAMSUNG", "I8150", "26/10/2012")
+		self.venda.fazer_venda()
+		self.venda.lista_vendas_1[0].nome_cli |should| equal_to("Ela")
+		self.venda.lista_vendas_1[0].descricao_prod |should| equal_to('Samsung Galaxy')
+		self.venda.consultar_garantia("26/10/2012") |should| equal_to("Ainda está na garantia!")
 
 	def test_esta_fora_da_garantia(self):
-		venda = Venda("Ela", "Casa dela","Samsung Galaxy", "SAMSUNG", "I8150", "26/10/2012")
-		venda.consultar_garantia("26/10/2013") |should| equal_to("Ainda está na garantia!")
+		self.venda = Venda("Ela", "Casa dela","Samsung Galaxy", "SAMSUNG", "I8150", "26/10/2012")
+		self.venda.fazer_venda()
+		self.venda.consultar_garantia("26/10/2016") |should| equal_to("Esta fora da garantia")
 
 
 
